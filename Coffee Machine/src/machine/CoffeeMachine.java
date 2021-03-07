8af6fb4381e10e77a4315cbbd135fa3295d73dd4
bob-qian <bobqian2002@gmail.com>
2021-03-07 14:28:30 -0500

    completed stage 3 of coffee machine

--- Coffee Machine/src/machine/CoffeeMachine.java
+++ Coffee Machine/src/machine/CoffeeMachine.java
@@ -14,16 +14,49 @@ public class CoffeeMachine {
 
         Scanner scnr = new Scanner(System.in);
 
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int inputWater = scnr.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int inputMilk = scnr.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int inputBeans = scnr.nextInt();
+
+        // Initialize a new Coffee Machine
+        CoffeeMachine coffee = new CoffeeMachine(inputWater, inputMilk, inputBeans);
+
         System.out.println("Write how many cups of coffee you will need:");
         int cupsCoffee = scnr.nextInt();
-        calcIngredients(cupsCoffee);
+
+        System.out.println(coffee.makeCups(cupsCoffee));
+
 
     }
 
-    public static void calcIngredients(int cupsCoffee) {
-        System.out.printf("For %s cups of coffee you will need:\n", cupsCoffee);
-        System.out.printf("%d ml of water\n", cupsCoffee * 200);
-        System.out.printf("%d ml of milk\n", cupsCoffee * 50);
-        System.out.printf("%d g of coffee beans\n", cupsCoffee * 15);
+    private int waterAmt;
+    private int milkAmt;
+    private int beansAmt;
+
+    public CoffeeMachine(int waterAmt, int milkAmt, int beansAmt) {
+        this.waterAmt = waterAmt;
+        this.milkAmt = milkAmt;
+        this.beansAmt = beansAmt;
+    }
+
+    public String makeCups(int cupsCoffee) {
+        int waterCanProduce = this.waterAmt / 200; // How many cups of coffee could be produced
+        int milkCanProduce = this.milkAmt / 50; // How many cups of coffee could be produced
+        int beansCanProduce = this.beansAmt / 15; // How many cups of coffee could be produced
+
+        int cupsMade = Math.min(Math.min(waterCanProduce, milkCanProduce), beansCanProduce);
+
+        if (cupsMade == cupsCoffee) {
+            return "Yes, I can make that amount of coffee";
+        } else if (cupsMade > cupsCoffee) {
+            return "Yes, I can make that amount of coffee (and even " +
+                    (cupsMade - cupsCoffee) + " more than that)";
+        }
+        return "No, I can make only " + (cupsMade) + " cup(s) of coffee";
     }
 }
