040f1bd9b7f2f92a47ced5b140a7cec586a09041
bob-qian <bobqian2002@gmail.com>
2021-03-07 14:56:44 -0500

    changed scnr to private static

--- Tic-Tac-Toe/src/tictactoe/Main.java
+++ Tic-Tac-Toe/src/tictactoe/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public final Scanner scnr = new Scanner(System.in);
+    private static Scanner scnr = new Scanner(System.in);
 
     public static void main(String[] args) {
 
