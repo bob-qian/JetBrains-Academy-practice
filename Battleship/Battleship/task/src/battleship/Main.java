032cfb21ee8fa66f10cdd4301904eb9e3c01bea4
bob-qian <bobqian2002@gmail.com>
2021-03-31 19:12:51 -0500

    began refactoring code into methods, adding javadoc

--- Battleship/Battleship/task/src/battleship/Main.java
+++ Battleship/Battleship/task/src/battleship/Main.java
@@ -1,32 +1,26 @@
+/**
+ * A class to run the Battleship game.
+ * @author Bob Qian
+ * @version 1.0
+ */
+
 package battleship;
 
 import java.util.*;
 
 public class Main {
 
+    public static final Scanner scnr = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
 
         BattleshipMap userMap = new BattleshipMap();
         System.out.println(userMap);
 
-        // Ask for an input for each type of ship
-        String processStatus = "";
-        for (String shipType : shipsAndLengths.keySet()) {
-            System.out.println("Enter the coordinates of the " +
-                    shipType + " (" + shipsAndLengths.get(shipType) + " cells):\n");
-            do {
-                String[] userCoordinates = scnr.nextLine().split(" ");
-                processStatus = userMap.addShip(userCoordinates, shipType);
-                if (!processStatus.equals("success")) {
-                    System.out.println("\n" + processStatus + " Try again:\n");
-                }
-            } while (!processStatus.equals("success"));
-            System.out.println("\n" + userMap);
-        }
-
+        setUpShips(userMap);
+        
         // Clear the process status for next loop, taking shots
-        processStatus = "";
+        String processStatus = "";
         System.out.println("The game starts!\n");
         BattleshipMap opponentShootingMap = new BattleshipMap();
         System.out.println(opponentShootingMap);
@@ -44,7 +38,30 @@ public class Main {
         System.out.println("\n" + userMap);
     }
 
+    /**
+     * Loops through all 5 ships and takes user input to set location for each.
+     * @param inputMap the BattleshipMap object for the ships to be created and stored in
+     */
+    public static void setUpShips(BattleshipMap inputMap) {
+        String processStatus = "";
+
+        // Ask for an input for each type of ship
+        for (String shipType : shipsAndLengths.keySet()) {
+            System.out.println("Enter the coordinates of the " +
+                    shipType + " (" + shipsAndLengths.get(shipType) + " cells):\n");
+            do {
+                String[] userCoordinates = scnr.nextLine().split(" ");
+                processStatus = inputMap.addShip(userCoordinates, shipType);
+                if (!processStatus.equals("success")) {
+                    System.out.println("\n" + processStatus + " Try again:\n");
+                }
+            } while (!processStatus.equals("success"));
+            System.out.println("\n" + inputMap);
+        }
+    }
+
     private static LinkedHashMap<String, Integer> shipsAndLengths = new LinkedHashMap<>();
+
 
     private static class BattleshipMap {
         static final String[] rows = new String[]{"A", "B", "C", "D", "E",
@@ -53,12 +70,13 @@ public class Main {
                 "6", "7", "8", "9", "10" };
         private HashMap<String, String> map;
 
+        /**
+         * Constructor initializing a BattleshipMap object.
+         * Creates internal map that has no ships, only "~".
+         */
         public BattleshipMap() {
-            shipsAndLengths.put("Aircraft Carrier", 5);
-            shipsAndLengths.put("Battleship", 4);
-            shipsAndLengths.put("Submarine", 3);
-            shipsAndLengths.put("Cruiser", 3);
-            shipsAndLengths.put("Destroyer", 2);
+            //TODO Need to try to use ENUMS
+            shipEnums();
 
             map = new HashMap<>();
             // Populate the battleship map
@@ -69,10 +87,31 @@ public class Main {
             }
         }
 
+        //TODO Need to try to use ENUMS
+        public void shipEnums() {
+            shipsAndLengths.put("Aircraft Carrier", 5);
+            shipsAndLengths.put("Battleship", 4);
+            shipsAndLengths.put("Submarine", 3);
+            shipsAndLengths.put("Cruiser", 3);
+            shipsAndLengths.put("Destroyer", 2);
+        }
+
+        /**
+         * Gets internal map for each BattleshipMap object.
+         * @return the internal HashMap map for each BattleshipMap object.
+         */
         public HashMap<String, String> getMap() {
             return map;
         }
 
+        /**
+         * Takes a "shot" at the specified cell location. This updates the "hidden"
+         * map of the BattleshipMap object it is called on. The shot is represented
+         * to the shooter on the visible map (opponentShootingMap).
+         * @param cell the cell location of the "shot"
+         * @param opponentShootingMap visible map that will show shot location
+         * @return String denoting succesful shot ("success") or an error message.
+         */
         public String takeShot(String cell, BattleshipMap opponentShootingMap) {
             String cellBeingShot = map.get(cell);
 
@@ -227,6 +266,10 @@ public class Main {
             return "success";
         }
 
+        /***
+         * Returns a string representation of the BattleshipMap.
+         * @return a string representation of the BattleshipMap.
+         */
         @Override
         public String toString() {
             StringBuilder displayedMap = new StringBuilder(" ");
