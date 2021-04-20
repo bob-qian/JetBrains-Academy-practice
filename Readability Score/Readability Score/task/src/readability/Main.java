14971f8bb864025d03080e9c662ff52682c72e76
bob-qian <bobqian2002@gmail.com>
2021-04-20 11:11:02 -0400

    wrote textDifficulty(), completed stage 1

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -1,7 +1,24 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scnr = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        String input = scnr.nextLine();
+        System.out.println(textDifficulty(input));
+    }
+
+    /**
+     * Determines a text difficulty ("EASY" or "HARD") based on symbol count
+     * @param text
+     * @return
+     */
+    public static String textDifficulty(String text) {
+        if (text.length() > 100) {
+            return "HARD";
+        }
+        return "EASY";
     }
 }
