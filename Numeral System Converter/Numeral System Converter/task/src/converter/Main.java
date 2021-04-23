f16d7d0bb145ec51269969043e936f21e6054b56
bob-qian <bobqian2002@gmail.com>
2021-04-23 17:23:55 -0400

    completed stage 4

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -15,15 +15,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        long userInput = Long.parseLong(scnr.nextLine());
-        int userRadix = Integer.parseInt(scnr.nextLine());
+        int sourceRadix = Integer.parseInt(scnr.nextLine());
 
-        System.out.println(convertToBaseRadix(userInput, userRadix));
+        int sourceNumberBase10;
+        if (sourceRadix == 1) {
+            // Special case
+            String sourceNumberInput = scnr.nextLine();
+            sourceNumberBase10 = sourceNumberInput.length();
+        } else {
+            // Directly convert input into base 10 representation, regardless of original radix/base
+            sourceNumberBase10 = Integer.parseInt(scnr.nextLine(), sourceRadix);
+        }
+
+        int targetRadix = Integer.parseInt(scnr.nextLine());
+
+        StringBuilder output = new StringBuilder();
+
+        if (targetRadix == 1) {
+            // Special case
+            for (int i = 0; i < sourceNumberBase10; i++) {
+                output.append(1);
+            }
+        } else {
+            output.append(convertToBaseRadix(sourceNumberBase10, targetRadix));
+        }
+        System.out.println(output.toString());
+
     }
 
-    // Converts a number to base -radix- and then appends a prefix; returns a String
-    private static String convertToBaseRadix(Long number, int radix) {
-        String output = Long.toString(number, radix);
-        return prefixes.get(radix) + output;
+    // Converts a number to base -radix-; returns a String
+    private static String convertToBaseRadix(int number, int radix) {
+        String output = Integer.toString(number, radix);
+        return output;
     }
 }
